83fac1bdb7660dc27b80ef46f7b88aaca64838b5
Roman_Florina <87187465+Styx9@users.noreply.github.com>
2025-03-11 12:22:47 +0300

    Update main.cpp

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -5,30 +5,30 @@ using namespace std;
 
 int main()
 {
-    cout << "Add(2, 3) = " << Math::Add(2, 3) << std::endl;
-    cout << "Add(2, 3, 4) = " << Math::Add(2, 3, 4) << std::endl;
+    cout << "Add(2, 3) = " << Math::Add(2, 3) << endl;
+    cout << "Add(2, 3, 4) = " << Math::Add(2, 3, 4) << endl;
 
-    cout << "Add(2.5, 3.5) = " << Math::Add(2.5, 3.5) << std::endl;
-    cout << "Add(2.5, 3.5, 4.5) = " << Math::Add(2.5, 3.5, 4.5) << std::endl;
+    cout << "Add(2.5, 3.5) = " << Math::Add(2.5, 3.5) << endl;
+    cout << "Add(2.5, 3.5, 4.5) = " << Math::Add(2.5, 3.5, 4.5) << endl;
 
-    cout << "Mul(2, 3) = " << Math::Mul(2, 3) << std::endl;
-    cout << "Mul(2, 3, 4) = " << Math::Mul(2, 3, 4) << std::endl;
+    cout << "Mul(2, 3) = " << Math::Mul(2, 3) << endl;
+    cout << "Mul(2, 3, 4) = " << Math::Mul(2, 3, 4) << endl;
 
-    cout << "Mul(1.5, 2.5) = " << Math::Mul(1.5, 2.5) << std::endl;
-    cout << "Mul(1.5, 2.5, 3.5) = " << Math::Mul(1.5, 2.5, 3.5) << std::endl;
+    cout << "Mul(1.5, 2.5) = " << Math::Mul(1.5, 2.5) << endl;
+    cout << "Mul(1.5, 2.5, 3.5) = " << Math::Mul(1.5, 2.5, 3.5) << endl;
 
-    cout << "Add(5 numbers: 1, 2, 3, 4, 5) = " << Math::Add(5, 1, 2, 3, 4, 5) << std::endl;
+    cout << "Add(5 numbers: 1, 2, 3, 4, 5) = " << Math::Add(5, 1, 2, 3, 4, 5) << endl;
 
     const char* str1 = "Hello, ";
     const char* str2 = "world!";
 
     char* concatenated = Math::Add(str1, str2);
     if (concatenated) {
-        std::cout << "String Add: " << concatenated << std::endl;
-        delete[] concatenated; // Free allocated memory
+        cout << "String Add: " << concatenated << endl;
+        delete[] concatenated; 
     }
     else {
-        std::cout << "String Add returned nullptr" << std::endl;
+        cout << "String Add returned nullptr" << endl;
     }
 
     return 0;
